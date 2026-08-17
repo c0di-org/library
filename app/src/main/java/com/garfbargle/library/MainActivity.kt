10b4674ec43e9605864b1c4ac03906fd373598ee
garfbargle <o.codi.o@gmail.com>
2026-08-16 20:01:08 -0600

    Respect top safe area across Library screens

--- app/src/main/java/com/garfbargle/library/MainActivity.kt
+++ app/src/main/java/com/garfbargle/library/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.ui.Modifier
 import com.garfbargle.library.ui.LibraryApp
+import com.garfbargle.library.ui.theme.Ink
 import com.garfbargle.library.ui.theme.LibraryTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +19,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LibraryTheme {
-                LibraryApp()
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .background(Ink)
+                        .statusBarsPadding()
+                ) {
+                    LibraryApp()
+                }
             }
         }
     }
